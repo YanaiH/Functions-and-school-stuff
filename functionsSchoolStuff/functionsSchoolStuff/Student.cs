1c6ab94cbd695d84f7eb8f5daebcb9359e961453
YanaiH <yhyh0203@gmail.com>
2019-05-27 09:36:28 +0300

    Added student class and first functions to it.

---
+++ functionsSchoolStuff/functionsSchoolStuff/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace functionsSchoolStuff
+{
+    class Student
+    {
+        private string name;
+        private int[] grades;
+        private int numOfGrades;
+
+        public Student(string name)
+        {
+            if (name.Length>0)
+            {
+                this.name = name;
+            }
+            grades = new int[100];
+            numOfGrades = 0;
+        }
+
+        public void AddGrade(int grade)
+        {
+            if (grade > 0 && grade < 100)
+            {
+                grades[numOfGrades] = grade;
+                numOfGrades++;
+            }
+        }
+
+        public double Average()
+        {
+            int sum = 0;
+            for (int i = 0; i < numOfGrades; i++)
+            {
+                sum = sum + grades[i];
+            }
+            return sum / numOfGrades;
+        }
+    }
+}
