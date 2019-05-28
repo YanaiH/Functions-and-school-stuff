08a0867634ec4e3dc01bebcb8c315deec4e3dfe6
YanaiH <yhyh0203@gmail.com>
2019-05-28 14:04:20 +0300

    Added FatBoye class and main to experiment with object arrays.

--- functionsSchoolStuff/functionsSchoolStuff/Program.cs
+++ functionsSchoolStuff/functionsSchoolStuff/Program.cs
@@ -4,7 +4,7 @@ namespace functionsSchoolStuff
 {
     class Program
     {
-        static void Main(string[] args) // just for testing the IsEachNumOnce function
+        static void Main1(string[] args) // just for testing the IsEachNumOnce function
         {
             int[] test = { 1, 2, 3, 4, 5, 6, 7, 8 };
             if (MyMath.IsEachNumOnce(test))
@@ -18,5 +18,16 @@ namespace functionsSchoolStuff
             Console.ReadKey();
         }
 
+        static void Main()
+        {
+            FatBoye[] fatBoyes = new FatBoye[3];
+            for (int i = 0; i < fatBoyes.Length; i++)
+            {
+                fatBoyes[i] = new FatBoye();
+                fatBoyes[i].SetFat(true);
+                Console.WriteLine(fatBoyes[i].Name());
+            }
+            Console.ReadKey();
+        }
     }
 }
